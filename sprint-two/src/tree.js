db5576ea15f448665d297fc7d41be177dfc9f53c
smmakowski <makowski.sm@gmail.com>
2017-02-09 11:23:16 -0800

    "Determined time complexity for Tree"

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -13,28 +13,31 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
-	var child = Tree(value);
-	this.children.push(child);
+  var child = Tree(value);
+  this.children.push(child);
 };
 
 treeMethods.contains = function(target, node) {
-	var found = false;
-	node = node || this;
-	if (node.value === target) {
-		return true;
-	} else if (node.children.length > 0){
-		for(var i = 0; i < node.children.length; i++) {
-			if (found){
-				break;
-			}
-			found = found || node.contains(target, node.children[i]);
-		}
-	}
-	return found;
+  var found = false;
+  node = node || this;
+  if (node.value === target) {
+    return true;
+  } else if (node.children.length > 0) {
+    for (var i = 0; i < node.children.length; i++) {
+      if (found) {
+        break;
+      }
+
+      found = found || node.contains(target, node.children[i]);
+    }
+  }
+  return found;
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  addChild: constant O(1)
+  contains: Linear
  */
