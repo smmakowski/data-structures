9bd9cd27e8b64182185cbc460474e8b2dd318fe7
smmakowski <makowski.sm@gmail.com>
2017-02-08 19:34:32 -0800

    Passed all tests for all patterns for queue and stack.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.line = {};
+  this.length = 0;
+  this.dqc = 0;
+  this.eqc = 0;
+
+};
+
+Queue.prototype.enqueue = function(value) {
+  this.line[this.eqc] = value;
+  this.eqc++;
+  this.length++;
+};
+
+Queue.prototype.dequeue = function() {
+  var dqd = this.line[this.dqc];
+  delete this.line[this.dqc];
+  this.dqc++;
+  if (this.length > 0) {
+    this.length--;
+  }
+  return dqd;
+};
+
+Queue.prototype.size = function( ) {
+  return this.length;
 };
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -8,26 +8,25 @@ var Stack = function() {
 
 };
 
-Stack.prototype = {
-  push : function(value) {
-    this.storage[this.length] = value;
-    this.length++;
-  },
 
-  pop : function() {
-    var ppd = this.storage[this.length - 1];
-    delete this.storage[this.length - 1];
-
-    if (this.length > 0) {
-      this.length--;
-    }
-
-    return ppd;
-  },
-
-  size : function() {
-    return this.length;
-  }
+Stack.prototype.push = function(value) {
+  this.storage[this.length] = value;
+  this.length++;
 };
+
+Stack.prototype.pop = function() {
+  var ppd = this.storage[this.length - 1];
+  delete this.storage[this.length - 1];
+
+  if (this.length > 0) {
+    this.length--;
+  }
+  return ppd;
+};
+
+Stack.prototype.size = function() {
+  return this.length;
+};
+
 
 
