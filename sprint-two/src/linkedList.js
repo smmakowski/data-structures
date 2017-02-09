9d32100e0ba41212a51892631cd7f2e077e00818
smmakowski <makowski.sm@gmail.com>
2017-02-08 20:37:49 -0800

    Passed tests for linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,43 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = Node(value);
+    list[value] = node;
+    
+    // nothing in the list
+    if (list.head === null && list.tail === null) {
+      list.tail = node;
+      list.head = node;
+    } else {
+      list.tail = node;
+      list.head.next = node;
+    }
+
+    // if there stuff in the list
+    
   };
 
   list.removeHead = function() {
+    var remVal = list.head.value;
+    var remHead = list.head;
+    
+    delete list.head;
+
+    list.head = remHead.next;
+
+    return remVal;
   };
 
-  list.contains = function(target) {
+  list.contains = function(target, node) {
+    node = node || list.head;
+    if (node.value === target) {
+      return true;
+    } else if (node.next === null) {
+      return false;
+    } else {
+      var nextNode = node.next;
+      return list.contains(target, nextNode);     
+    }
   };
 
   return list;
