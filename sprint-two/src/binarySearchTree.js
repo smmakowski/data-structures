83620bbd27342f83aca57762f52a29146130cb6e
smmakowski <makowski.sm@gmail.com>
2017-02-09 20:44:59 -0800

    Partial commit.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,42 @@
 var BinarySearchTree = function(value) {
+  //debugger;
+  var bstObj = Object.create(bstMethods);
+  bstObj.value = value;
+  bstObj.left = null;
+  bstObj.right = null;
+
+  return bstObj;
+};
+
+var bstMethods = {};
+
+bstMethods.insert = function(value, node) {
+  node = node || this;
+  if (node.value < value) {
+    if (node.left === null) {
+      var node = BinarySearchTree(value);
+      node.left = node;
+      return;
+    } else {
+      this.insert(value, this.left);
+    }
+  }
+};
+
+bstMethods.contains = function(value) {
+
+};
+
+bstMethods.depthFirstLog = function(cb) {
+
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+/*
+8, 3
+
+*/
+
